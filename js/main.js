1d7f19ad3193af18163ecc6c30b56848c8ff4c5b
Yukiji Nakatsuka <lunarbase@outlook.com>
2018-08-02 22:12:53 -0700

    add function in javascript

--- js/main.js
+++ js/main.js
@@ -9,3 +9,26 @@ $('span').click(function() {
   $('.modal').css('display', 'none')
   console.log('running!')
 })
+
+
+
+function bookSearch() {
+  var search = document.getElementById('search').value
+  var modalContent = document.getElementsByClassName('modal-content')[0]
+
+  console.log(search)
+
+  $.ajax({
+    url: "https://www.googleapis.com/books/v1/volumes?q=" + search, //what ever user added
+    dataType: "json",
+
+    success: function(data) {
+      for (var i = 0; i < data.items.length; i++) {
+        modalContent.innerHTML += "<h2>" + data.items[i].volumeInfo.title + "</h2>"
+        }
+      },
+      type: 'GET'
+    }) //end of ajax
+  } // end of function booksearch()
+
+document.getElementById('myBtn').addEventListener('click', bookSearch, false)
